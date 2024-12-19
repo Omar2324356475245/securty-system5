6526954cd62675f3351926663814c68533287562
Omar2324356475245 <oi1772857@gmail.com>
2024-12-19 09:14:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const navLinks = document.querySelectorAll('.nav-links a');
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.transform = 'scale(1.1)';
+        link.style.transition = 'transform 0.3s ease';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.transform = 'scale(1)';
+    });
+});
